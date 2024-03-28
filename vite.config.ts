fe2b640821236af7db275d8cf3bb4cee5000fc10
Lazarus <vegetasssuper@gmail.com>
2024-03-28 22:22:26 +0000

    upd vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,20 @@
 import path from "path";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
-console.log(process.env);
 
+console.log(process.env);
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: process.env.NODE_ENV == "development" ? "/" : process.env.BASE_URL,
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  console.log(process.env);
+
+  return {
+    base: process.env.NODE_ENV == "development" ? "/" : process.env.BASE_URL,
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
+  };
 });
