214b47ccb1e84e43131199eed34ebbf1677c8b61
Lazarus <vegetasssuper@gmail.com>
2024-03-28 21:15:07 +0000

    env log

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+console.log(process.env);
 
 // https://vitejs.dev/config/
 export default defineConfig({
