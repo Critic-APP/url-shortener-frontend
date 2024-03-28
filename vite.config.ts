44ec3d64555058f027dbce6e01bda17af5d4204a
Lazarus <vegetasssuper@gmail.com>
2024-03-28 20:50:58 +0000

    base_url update test

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:
+    process.env.NODE_ENV == "development"
+      ? "/"
+      : process.env.RAILWAY_PUBLIC_DOMAIN,
   plugins: [react()],
   resolve: {
     alias: {
