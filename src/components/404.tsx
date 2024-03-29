a40d39f55c34699fc8a466c7a28734eb3c0e19ef
Lazarus <vegetasssuper@gmail.com>
2024-03-29 14:29:14 +0000

    404 resize

--- src/components/404.tsx
+++ src/components/404.tsx
@@ -4,14 +4,14 @@ export default function NoMatch() {
   return (
     <main className="flex min-h-screen flex-col p-4 sm:p-5 items-center justify-center">
       <section className="container -mt-5 px-4 py-7 flex flex-col items-center justify-center">
-        <div className="text-primary-header text-balance text-start md:text-center font-extrabold mx-auto">
+        <div className="text-primary-header text-balance text-center font-extrabold mx-auto">
           <h1 className="text-3xl sm:text-5xl lg:text-6xl">
             Don’t panic! <br /> We’re short on something right now. <br />
             We’ll be back soon.
           </h1>
         </div>
         <div className="flex-1 text-center mt-4 lg:mt-0 lg:ml-3">
-          <img className="w-52" src={page404} alt="Error 404 Image" />
+          <img className="w-72" src={page404} alt="Error 404 Image" />
         </div>
       </section>
     </main>
